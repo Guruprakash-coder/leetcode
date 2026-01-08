16bab063931a90c2c45a4188b1d824f83656be7d
Guruprakash-coder <guruprakash6999@gmail.com>
2026-01-08 21:18:59 +0530

    Time: 7 ms (5.35%), Space: 49.6 MB (5.05%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countOperations(int num1, int num2) {
+        return helper(num1,num2,0);
+    }
+    int helper(int num1,int num2,int step){
+        if(num1==0 || num2==0){
+            return step;
+        }
+        if(num1>num2){
+            return helper(num1-num2,num2,step+1);
+        }
+        return helper(num1,num2-num1,step+1);
+    }
+}
