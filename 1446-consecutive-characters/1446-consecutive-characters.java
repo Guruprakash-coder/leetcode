d0bd20da765c62dc7b20101237839972b95bec03
Guruprakash-coder <guruprakash6999@gmail.com>
2026-03-22 12:50:53 +0530

    Time: 1 ms (100%), Space: 43.5 MB (55.38%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.java
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -1,20 +1,17 @@
 class Solution {
     public int maxPower(String s) {
-        if(s.length()==1){
-            return 1;
-        }
-        int max=0;
-        int cnt=1;
-        for(int i=1;i<s.length();i++){
-            if(s.charAt(i)==s.charAt(i-1)){
+        int max = 1;
+        int cnt = 1;
+
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(i) == s.charAt(i-1)){
                 cnt++;
-                max=Math.max(cnt,max);
+            } else {
+                cnt = 1;
             }
-            if(s.charAt(i)!=s.charAt(i-1)){
-                max=Math.max(cnt,max);
-                cnt=1;
-            }
+            max = Math.max(max, cnt);
         }
+
         return max;
     }
 }
