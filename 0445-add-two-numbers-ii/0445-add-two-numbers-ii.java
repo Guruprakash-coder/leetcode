3d2b41f90e87a278ccb82c98b0c12dc01668c055
Guruprakash-coder <guruprakash6999@gmail.com>
2026-05-09 14:31:25 +0530

    Time: 1 ms (100%), Space: 46.7 MB (30.23%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -10,38 +10,42 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
-       Stack<Integer> s1=new Stack<>();
-       Stack<Integer> s2=new Stack<>();
-       while(l1!=null){
-        s1.push(l1.val);
-        l1=l1.next;
-       }
-        while(l2!=null){
-        s2.push(l2.val);
-        l2=l2.next;
-       }
-
+        l1=reverse(l1);
+        l2=reverse(l2);
        int carry=0;
-       ListNode head=null;
+       ListNode dummy=new ListNode(0);
+       ListNode temp=dummy;
 
-       while(!s1.isEmpty() || !s2.isEmpty() || carry!=0){
-            int sum=carry;
-            if(!s1.isEmpty()){
-                sum+=s1.pop();
-            }
-            if(!s2.isEmpty()){
-                sum+=s2.pop();
-            }
-            carry=sum/10;
-            ListNode node=new ListNode(sum%10);
-            node.next=head;
-            head=node;
-
+       while(l1!=null || l2!=null || carry!=0){
+        int sum=carry;
+        if(l1!=null){
+            sum+=l1.val;
+            l1=l1.next;
+        }
+        if(l2!=null){
+            sum+=l2.val;
+            l2=l2.next;
+        }
+        carry=sum/10;
+        ListNode node=new ListNode(sum%10);
+        temp.next=node;
+        temp=temp.next;
        }
-        return head;
+       return reverse(dummy.next);
 
 
+
+    }
+    ListNode reverse(ListNode head){
+        if(head==null || head.next==null){
+            return head;
+        }
+
+        ListNode newHead=reverse(head.next);
+        
+        head.next.next=head;
+        head.next=null;
+        return newHead;
 
     }
 }
