6ea6268d659bf5fb54ddd4a53edcb29fbf9b69e2
Guruprakash <guruprakash6999@gmail.com>
2026-06-03 15:34:00 +0530

    Time: 1 ms (100%), Space: 47 MB (24.22%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,17 +1,20 @@
 class Solution {
     public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
         int ans=Integer.MAX_VALUE;
+        int WMIN=Integer.MAX_VALUE;
+        int LMIN=Integer.MAX_VALUE;
+
         for(int i=0;i<landStartTime.length;i++){
-            int t=landStartTime[i]+landDuration[i];
-            for(int j=0;j<waterStartTime.length;j++){
-                ans=Math.min(ans,Math.max(t,waterStartTime[j])+waterDuration[j]);
-            }
+            LMIN=Math.min(LMIN,landStartTime[i]+landDuration[i]);
         }
         for(int i=0;i<waterStartTime.length;i++){
-            int t=waterStartTime[i]+waterDuration[i];
-            for(int j=0;j<landStartTime.length;j++){
-                ans=Math.min(ans,Math.max(t,landStartTime[j])+landDuration[j]);
-            }
+            ans=Math.min(ans,Math.max(LMIN,waterStartTime[i])+waterDuration[i]);
+        }
+        for(int i=0;i<waterStartTime.length;i++){
+            WMIN=Math.min(WMIN,waterStartTime[i]+waterDuration[i]);
+        }
+        for(int i=0;i<landStartTime.length;i++){
+            ans=Math.min(ans,Math.max(WMIN,landStartTime[i])+landDuration[i]);
         }
         return ans;
     }
