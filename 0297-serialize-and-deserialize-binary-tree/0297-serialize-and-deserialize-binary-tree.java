9b8bad6eeaa81805a6add61f60833e3608f265e6
Guruprakash <guruprakash6999@gmail.com>
2026-06-12 15:16:41 +0530

    Time: 8 ms (87.16%), Space: 49 MB (6.26%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+    
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb=new StringBuilder();
+        fillSb(root,sb);
+        System.out.println(sb);
+        return sb.toString();
+    }
+    private void fillSb(TreeNode node,StringBuilder sb){
+        if(node==null){
+            
+            sb.append("N,");
+            
+            return;
+        }
+        sb.append(node.val).append(',');
+        
+        fillSb(node.left,sb);
+        fillSb(node.right,sb);
+
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] arr=data.split(",");
+        return helper(arr);
+    }
+    int index=0;
+    private TreeNode helper(String[] arr){
+        
+        if(arr[index].equals("N")){
+            return null;
+        }
+        TreeNode node=new TreeNode(Integer.parseInt(arr[index]));
+        index++;
+        node.left=helper(arr);
+        index++;
+        node.right=helper(arr);
+        
+        return node;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
