4410234e9a86c25cf7fdc1ab1d21bef0f5538899
Guruprakash <guruprakash6999@gmail.com>
2026-07-02 00:30:34 +0530

    Time: 290 ms (48.41%), Space: 103.8 MB (33.91%) - LeetHub

--- 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.java
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.java
@@ -1,28 +1,26 @@
 class Solution {
     public int halveArray(int[] nums) {
-        PriorityQueue<Double> pq=new PriorityQueue<>(Collections.reverseOrder());
-        long sum=0;
-        for(int n:nums){
-            pq.add((double)n);
-            sum+=n;
-        }
-        
+    long sum = 0;
+PriorityQueue<Double> pq = new PriorityQueue<>(Collections.reverseOrder());
 
-        double min=sum/2.0;
-        
-        int ans=0;
-        double running=0;
-        while(!pq.isEmpty()){
-            if(sum-running<=min){
-                break;
-            }
-            double n=pq.poll()/2.0;
-            running+=n;
-            ans++;
-            pq.add(n);
-            
-        }
-        return ans;
+for (int num : nums) {
+    sum += num;
+    pq.offer((double) num);
+}
+
+double target = sum / 2.0;
+double reduced = 0;
+int operations = 0;
+
+while (reduced < target) {
+    double largest = pq.poll();
+    largest /= 2.0;
+    reduced += largest;
+    pq.offer(largest);
+    operations++;
+}
+
+return operations;
 
     }
 }
