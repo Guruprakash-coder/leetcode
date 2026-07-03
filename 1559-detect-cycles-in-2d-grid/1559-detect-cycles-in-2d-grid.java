3837f6bf61a9a6d529c0ec1b3d619e9025864731
Guruprakash <guruprakash6999@gmail.com>
2026-07-03 23:51:29 +0530

    Time: 24 ms (21.22%), Space: 120 MB (99.55%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -1,33 +1,43 @@
 class Solution {
-    private static final int[][] DIR={
-        {1,0} ,{-1,0},{0,1},{0,-1}
+    private static final int[][] DIR = {
+         {1,0} ,{-1,0},{0,1},{0,-1}
     };
     public boolean containsCycle(char[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        boolean[] vis=new boolean[m*n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(!vis[i*n+j]){
-                    boolean check=dfs(vis,grid,i,j,-1,-1,m,n);
-                    if(check) return true;
+        boolean[][] vis=new boolean[grid.length][grid[0].length];
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(!vis[i][j]){
+                    if(bfs(i,j,grid,vis)) return true;
                 }
             }
         }
         return false;
     }
-    private boolean dfs(boolean[] visit,char[][] grid,int r,int c,int pr , int pc,int m,int n){
-        visit[r * n + c] = true;
-        for (int[] d : DIR) {
-            int nr = r + d[0];
-            int nc = c + d[1];
-            if (nr != pr || nc != pc)
-                if (nr >= 0 && nr < m && nc >= 0 && nc < n)
-                    if (grid[nr][nc] == grid[r][c])
-                        if (visit[nr * n + nc] || dfs(visit,grid,nr, nc, r, c, m, n))
-                            return true;
+    private boolean bfs(int r,int c,char[][] grid,boolean[][] vis){
+        vis[r][c]=true;
+        char ch=grid[r][c];
+        Queue<int[]> q=new LinkedList<>();
+        q.offer(new int[]{r,c,-1,-1});
+        while(!q.isEmpty()){
+            int[] nodes=q.poll();
+            int[] curr={nodes[0],nodes[1]};
+            int[] parent={nodes[2],nodes[3]};
+            for(int[] D:DIR){
+                int[] node={curr[0]+D[0],curr[1]+D[1],curr[0],curr[1]};
+                if(node[0]<0 || node[1]<0 || node[0]>=grid.length || node[1]>=grid[0].length ){
+                    continue;
+                }
+                if(grid[node[0]][node[1]]!=ch){
+                    continue;
+                }
+                if(node[0]==parent[0] && node[1]==parent[1]){
+                    continue;
+                };
+                if(vis[node[0]][node[1]]) return true;
+                vis[node[0]][node[1]]=true;
+                q.offer(node);
+            }
         }
         return false;
-
     }
 }
