8b48acac02e89e835f55d0620360456c9148f573
Guruprakash <guruprakash6999@gmail.com>
2026-07-13 09:36:55 +0530

    Time: 1 ms (28.06%), Space: 42.5 MB (22.13%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -1,24 +1,29 @@
 class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
-        int[] seqDigits = {
-            12, 23, 34, 45, 56, 67, 78, 89,
-            123, 234, 345, 456, 567, 678, 789,
-            1234, 2345, 3456, 4567, 5678, 6789,
-            12345, 23456, 34567, 45678, 56789,
-            123456, 234567, 345678, 456789,
-            1234567, 2345678, 3456789,
-            12345678, 23456789,
-            123456789
-        };
+        List<Integer> nums=new ArrayList<>();
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=1;i<=9;i++){
+            q.offer(i);
+        }
+        while(!q.isEmpty()){
+            int num=q.poll();
 
+            int last=num%10;
+            if(last<9){
+                int next=num*10+last+1;
+                nums.add(next);
+                q.offer(next);
+            }
+            
+        }
         List<Integer> ans = new ArrayList<>();
-
-        for (int num : seqDigits) {
+        for (int num : nums) {
             if (num >= low && num <= high) {
                 ans.add(num);
             }
         }
 
         return ans;
+
     }
 }
