44783fa6425d590e18091688579b20fc5595bf5a
Guruprakash <guruprakash6999@gmail.com>
2026-07-16 23:53:58 +0530

    Time: 104 ms (51.66%), Space: 50.8 MB (93.77%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countPrimes(int n) {
+        int ans=0;
+        boolean[] notPrime=new boolean[n];
+        for(int i=2;i<n;i++){
+            if(!notPrime[i]){
+                ans++;
+                for(int j=1;i*j<n;j++){
+                    notPrime[i*j]=true;
+                }
+            }
+        }
+        return ans;
+    }
+}
