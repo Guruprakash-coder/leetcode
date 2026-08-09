488e7d4404fd2922f3be30d8a87312bfee64d57d
Guruprakash <guruprakash6999@gmail.com>
2026-08-10 00:33:41 +0530

    Time: 3 ms (9.85%), Space: 43.4 MB (6.86%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -0,0 +1,40 @@
+class Solution {
+    class Pair{
+        String first;
+        int lev;
+        Pair(String first,int lev){
+            this.first=first;
+            this.lev=lev;
+        }
+    }
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        Set<String> s=new HashSet<>();
+        for(String st:bank){
+            s.add(st);
+        }
+        Queue<Pair> q=new LinkedList<>();
+        q.offer(new Pair(startGene,0));
+        s.remove(startGene);
+        while(!q.isEmpty()){
+            Pair node=q.poll();
+            String gen=node.first;
+            int mut=node.lev;
+            if(gen.equals(endGene)) return mut;
+            for(int i=0;i<gen.length();i++){
+                char[] charArray=gen.toCharArray();
+                char orig=charArray[i];
+                for(char ch='A';ch<='Z';ch++){
+                    charArray[i]=ch;
+                    String newg=new String(charArray);
+                    if(s.contains(newg)){
+                        q.offer(new Pair(newg,mut+1));
+                        s.remove(newg);
+                        
+                    }
+                }
+                charArray[i]=orig;
+            }
+        }
+        return -1;
+    }
+}
