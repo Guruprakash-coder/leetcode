45f92f834864d0c5c7f976892e2a14fc1cdd6d34
Guruprakash <guruprakash6999@gmail.com>
2026-08-27 21:32:49 +0530

    Time: 0 ms (100%), Space: 43.7 MB (11%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -5,19 +5,45 @@ class Solution {
         int m=obstacleGrid[0].length;
         if(obstacleGrid[n-1][m-1]==1 || obstacleGrid[0][0]==1) return 0;
         int[][] dp=new int[n][m];
-        
-        for(int[] d:dp){
-            Arrays.fill(d,-1);
+        dp[0][0]=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(obstacleGrid[i][j]==1) {
+                    dp[i][j]=0;
+                    continue;
+                }
+                if(i==0 && j==0){
+                    continue;
+                }
+                int up=i>0?dp[i-1][j]:0;
+                int left=j>0?dp[i][j-1]:0;
+
+                dp[i][j]=up+left;
+            }
         }
+        return dp[n-1][m-1];
 
-        return f(obstacleGrid,dp,n-1,m-1);
+        
     }
-    private int f(int[][] obstacleGrid,int[][] dp,int r,int c){
-        if(r==0 && c==0) return 1;
-        if(r<0 || c<0 || obstacleGrid[r][c]==1  ) return 0;
-        if(dp[r][c]!=-1) return dp[r][c];
-        dp[r][c]=f(obstacleGrid,dp,r-1,c)+f(obstacleGrid,dp,r,c-1);
+    // public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        
+    //     int n=obstacleGrid.length;
+    //     int m=obstacleGrid[0].length;
+    //     if(obstacleGrid[n-1][m-1]==1 || obstacleGrid[0][0]==1) return 0;
+    //     int[][] dp=new int[n][m];
+        
+    //     for(int[] d:dp){
+    //         Arrays.fill(d,-1);
+    //     }
 
-        return dp[r][c];
-    }
+    //     return f(obstacleGrid,dp,n-1,m-1);
+    // }
+    // private int f(int[][] obstacleGrid,int[][] dp,int r,int c){
+    //     if(r==0 && c==0) return 1;
+    //     if(r<0 || c<0 || obstacleGrid[r][c]==1  ) return 0;
+    //     if(dp[r][c]!=-1) return dp[r][c];
+    //     dp[r][c]=f(obstacleGrid,dp,r-1,c)+f(obstacleGrid,dp,r,c-1);
+
+    //     return dp[r][c];
+    // }
 }
