4fb4fb8c7186cff48fcca428448470f36e9167d1
Guruprakash <guruprakash6999@gmail.com>
2026-08-27 21:40:57 +0530

    Time: 1 ms (99.84%), Space: 49.8 MB (94.31%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -2,34 +2,23 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        int[][] dist=new int[n][m];
-        for(int[] d:dist){
-            Arrays.fill(d,Integer.MAX_VALUE);
+        int[][] dp=new int[n][m];
+        for(int[] d:dp){
+            Arrays.fill(d,-1);
         }
-        dist[0][0]=grid[0][0];
-        PriorityQueue<int[]> pq=new PriorityQueue<>(
-            (x,y)->Integer.compare(x[0],y[0])
-        );
-        pq.offer(new int[]{grid[0][0],0,0});
-        while(!pq.isEmpty()){
-            int[] node=pq.poll();
-            int curdis=node[0];
-            int r=node[1];
-            int c=node[2];
-            if(curdis > dist[r][c]) continue;
-            if(r!=n-1){
-                if(curdis+grid[r+1][c] < dist[r+1][c]){
-                    dist[r+1][c]=curdis+grid[r+1][c];
-                    pq.offer(new int[]{curdis+grid[r+1][c],r+1,c});
-                }
-            }
-            if(c!=m-1){
-                if(curdis+grid[r][c+1] < dist[r][c+1]){
-                    dist[r][c+1]=curdis+grid[r][c+1];
-                    pq.offer(new int[]{curdis+grid[r][c+1],r,c+1});
-                }
-            }
+        return f(grid,dp,n-1,m-1);
+    }
+    private int f(int[][] grid,int[][] dp,int r,int c){
+        if(r==0 && c==0){
+            return grid[0][0];
         }
-        return dist[n-1][m-1];
+        if(r<0 || c<0) return 100000;
+        if(dp[r][c]!=-1) return dp[r][c];
+
+        int left=f(grid,dp,r,c-1)+grid[r][c];
+        int right=f(grid,dp,r-1,c)+grid[r][c];
+        dp[r][c]=Math.min(left,right);
+
+        return dp[r][c];
     }
 }
