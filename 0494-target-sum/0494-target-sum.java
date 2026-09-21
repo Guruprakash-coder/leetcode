9147180fab1a7857aa876c18f7ddce794d3b1cb6
Guruprakash <guruprakash6999@gmail.com>
2026-09-22 00:39:59 +0530

    Time: 34 ms (45.05%), Space: 47.2 MB (7.33%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,30 +1,29 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        
-        
-        int tot=0;
-        for(int n:nums){
-            tot+=n;
+        Map<Integer,Integer>[] dp=new HashMap[nums.length];
+        for(int i=0;i<nums.length;i++){
+            dp[i]=new HashMap<>();
         }
-        if(tot<target || (tot-target)%2!=0) return 0;
-        int[][] dp=new int[nums.length][(tot-target)/2+1];
-        for(int[] d:dp){
-            Arrays.fill(d,-1);
-        }
-        
-        return f(nums,dp,nums.length-1,(tot-target)/2);
+        return f(nums,dp,nums.length-1,target);
     }
-    private int f(int[] nums,int[][] dp,int ind,int rem){
-        if(ind==0){
-            if (rem == 0 && nums[0] == 0) return 2;
-            if(rem==0 || rem==nums[0]) return 1;
-            return 0;
+    private int f(int[] nums,Map<Integer,Integer>[] dp,int ind,int rem){
+        if (ind == 0) {
+            int ways = 0;
+
+            if (rem == nums[0])
+                ways++;
+
+            if (rem == -nums[0])
+                ways++;
+
+            return ways;
         }
-        if(dp[ind][rem]!=-1) return dp[ind][rem];
-        int notake=f(nums,dp,ind-1,rem);
-        int take=0;
-        if(nums[ind]<=rem) take=f(nums,dp,ind-1,rem-nums[ind]);
-        return dp[ind][rem]=notake+take;
+        if(dp[ind].containsKey(rem)){
+            return dp[ind].get(rem);
+        }
+        int add=f(nums,dp,ind-1,rem+nums[ind]);
+        int sub=f(nums,dp,ind-1,rem-nums[ind]);
+        dp[ind].put(rem,add+sub);
+        return dp[ind].get(rem);
     }
-    
 }
