74d53e0b4039859ea731869b8bce5aac0694b28b
Guruprakash <guruprakash6999@gmail.com>
2026-09-27 00:05:12 +0530

    Time: 0 ms (100%), Space: 41.9 MB (88.07%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -9,12 +9,13 @@ class Solution {
         return f(dp,n-1,n);
     }
     private int f(int[][] dp,int ind,int rem){
-        if(rem==0) return 1;
-        if(ind==0) return 0;
+        if(rem==0 || ind==0){
+            return 1;
+        }
         if(dp[ind][rem]!=-1) return dp[ind][rem];
         int notake=f(dp,ind-1,rem);
         int take=0;
-        if(rem>=ind) take=ind* f(dp,ind,rem-ind);
+        if(rem>=ind) take=ind * f(dp,ind,rem-ind);
         return dp[ind][rem]=Math.max(take,notake);
     }
 }
